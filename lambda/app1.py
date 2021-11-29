b56c652739eee6b3e9395220dd821cdc92495174
sristiraj <sristiraj.a@gmail.com>
2021-11-29 23:37:52 +0530

    Update app1.py

--- lambda/app1.py
+++ lambda/app1.py
@@ -17,7 +17,7 @@ s3_bucket_index = notify_config_path.replace("s3://","").find("/")
 s3_bucket = notify_config_path[5:s3_bucket_index+5]
 s3_key = notify_config_path[s3_bucket_index+6:]
 ses_client = boto3.client('ses',region_name=aws_region)
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -29,7 +29,7 @@ def exception_handler(e):
         'HTTPStatusCode': status_code,
         'error_message': json.dumps(str(e))
     }
-    
+
 def response_handler(response):
     status_code = response['ResponseMetadata']['HTTPStatusCode']
     return {
@@ -53,7 +53,7 @@ def details(payload):
     return loggroup, logstream, message    
 
 def send_email_notification(content):
-    
+
     response = ""
     try:
         content_notify = content["awslogs"]["data"]
@@ -64,32 +64,33 @@ def send_email_notification(content):
         print(log_payload)
         lgroup, lstream, message = details(log_payload)
 
-        # bucket = s3.Bucket(s3_bucket)
-        # objs = list(bucket.objects.filter(Prefix=s3_key))
-        objs = s3.list_objects(Bucket=s3_bucket, Prefix=s3_key)
-        print(objs)
-        for obj in objs["Contents"]:
-            bucket = s3_bucket
-            key = obj["Key"]
+        bucket = s3.Bucket(s3_bucket)
+        objs = list(bucket.objects.filter(Prefix=s3_key))
+        for obj in objs:
+            bucket = obj.bucket_name
+            key = obj.key
+            print(bucket)
+            print(key)
             if "." in key:
-                content_object = s3.get_object(Bucket=s3_bucket, Key=key)
+                content_object = s3.Object(bucket, key)
                 try:
-                    file_content = content_object['Body'].read().decode('utf-8')
+                    file_content = content_object.get()['Body'].read().decode('utf-8')
+                    print(file_content)
                     json_content = json.loads(file_content)
                 except Exception as e:
                     json_content = {"service_arn":"unknown-json"}
                     pass
-                
+
                 if json.loads(message)["service_arn"]==json_content["ServiceArn"]:
-                    
+
                     email_subject = "Automated notification mail: AWS Data Jobs: "+ json_content["ServiceArn"]
                     print(email_subject)
                     receipient_email = json_content["ReceiverEmail"]
                     sender_email = json_content["SenderEmail"]
                     receipient_emails = ", ".join(json_content["ReceiverEmail"])
-                    
+
                     logger.info("************************Send email notification method - Begin****************************")
-            
+
                     BODY_HTML = """\
                     <html>
                     <head>
@@ -104,22 +105,22 @@ def send_email_notification(content):
                     """
                     BODY_HTML += "<tr><td>"+message+"</td></tr>"
                     BODY_HTML += "</table></body></html>"
-            
+
                     print(f"***********************Body HTML is {BODY_HTML}**************************")
-                    
+
                     print(f"****************email subject is {email_subject}")
                     print(f"****************sender_email is {sender_email}")
                     print(f"****************receipient_email is {receipient_emails}")
-                   
+
                     # Create a multipart/mixed parent container.
                     msg = MIMEMultipart('mixed')
-                    
-                    
+
+
                     # Add subject, from and to lines.
                     msg['Subject'] = email_subject
                     msg['From'] = sender_email
                     msg['To'] = ", ".join(receipient_email)
-            
+
                     # Create a multipart/alternative child container.
                     msg_body = MIMEMultipart('alternative')
                     # The character encoding for the email.
@@ -132,11 +133,11 @@ def send_email_notification(content):
                     htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)        
                     # Add the text and HTML parts to the child container.
                     msg_body.attach(htmlpart)
-                    
+
                     # Attach the multipart/alternative child container to the multipart/mixed
                     # parent container.
                     msg.attach(msg_body)
-                    
+
                     #print(f"*******Type is {type(subject_area_receipient_email)}****************************")
                     #print("msg is: " + str(msg))
                     try:
@@ -146,7 +147,7 @@ def send_email_notification(content):
                             Destinations=receipient_email,
                             RawMessage={'Data':msg.as_string()}
                             )
-                        
+
                     # Display an error if something goes wrong.	
                     except ClientError as e:
                         logger.error(f"**************************[Failed] in send_email_notification {str(e)} ***********************")
@@ -165,10 +166,9 @@ def lambda_handler(event, context):
     notification_dict = {}
     item_dict = {}
     print(f"********Event is {event}************")
-    
-    response = send_email_notification(event)
-            
-    return {"result":response}
-      
 
-    
+    response = send_email_notification(event)
+
+    return {"result":response}
+
+
